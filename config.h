068304d278732824e801b861bdc2e2457e6d51df
Phil Thompson <phil@electricvisions.com>
2013-06-21 16:15:28 +0100

    switched to columns + nmaster2 as default layout. Added push up/down client patch. Tried to organise/remove shortcuts

--- config.h
+++ config.h
@@ -1,20 +1,22 @@
 /* See LICENSE file for copyright and license details. */
+#include "push.c"
 
 /* appearance */
 static const char font[]            = "sans:size=9:antialias=true:hinting=true";
 static const char normbordercolor[] = "#333333";
 static const char normbgcolor[]     = "#223355";
 static const char normfgcolor[]     = "#cccccc";
-static const char selbordercolor[]  = "#005588";
+static const char selbordercolor[]  = "#116699";
 static const char selbgcolor[]      = "#005588";
 static const char selfgcolor[]      = "#aaddee";
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const Bool showbar           = True;     /* False means no bar */
 static const Bool topbar            = True;     /* False means bottom bar */
 
 /* tagging */
-static const char *tags[] = { "dev", "web", "misc", "logs" };
+//static const char *tags[] = { "dev", "web", "misc", "logs" };
+static const char *tags[] = { "o" }; // Single view - no tags
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
@@ -27,17 +29,17 @@ static const Rule rules[] = {
 
 /* layout(s) */
 static const float mfact      = 0.66667; /* factor of master area size [0.05..0.95] */
-static const int nmaster      = 1;    /* number of clients in master area */
+static const int nmaster      = 2;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "TTT",      bstack },
-	{ "[]=",      tile },    /* first entry is default */
+	{ "|||",      col },
+//	{ "TTT",      bstack },
+//	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "||=",      bstackhoriz },
-	{ "|||",      col },
+//	{ "||=",      bstackhoriz },
 };
 
 /* key definitions */
@@ -56,49 +58,61 @@ static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, 
 static const char *termcmd[]  = { "rxvt", NULL };
 static const char *killcmd[]  = { "pkill", "-f", "startdwm", NULL };
 static const char *sublcmd[]  = { "subl", NULL };
-static const char *chromcmd[] = { "chromium-browser", "--disable-accelerated-compositing" NULL };
-static const char *foxcmd[] = { "firefox", NULL };
+static const char *chromcmd[] = { "google-chrome", "--disable-accelerated-compositing", NULL };
+//static const char *foxcmd[] = { "firefox", NULL };
 static const char *inkcmd[] = { "inkscape", NULL };
+static const char *gimpcmd[] = { "gimp", NULL };
 static const char *pcmanfmcmd[] = { "pcmanfm", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = pcmanfmcmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_t,      spawn,          {.v = sublcmd } },
-	{ MODKEY,                       XK_v,      spawn,          {.v = chromcmd } },
-	{ MODKEY,                       XK_f,      spawn,          {.v = foxcmd } },
-	{ MODKEY,                       XK_i,      spawn,          {.v = inkcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = killcmd } },
+
+// APPS
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },    // Terminal
+	{ MODKEY,                       XK_f,      spawn,          {.v = pcmanfmcmd } }, // File Browser
+	{ MODKEY,                       XK_t,      spawn,          {.v = sublcmd } },    // Text Editor
+	{ MODKEY,                       XK_w,      spawn,          {.v = chromcmd } },   // Web browser
+	{ MODKEY,                       XK_v,      spawn,          {.v = inkcmd } },     // Vector graphics
+	{ MODKEY,                       XK_r,      spawn,          {.v = gimpcmd } },    // Raster graphics
+
+// CLIENT MANIPULATION
+	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Down,   focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_Up,     focusstack,     {.i = -1 } },
+	{ MODKEY|ShiftMask,           	XK_Down,   pushdown,       {0} },
+  { MODKEY|ShiftMask,           	XK_Up,     pushup,         {0} },
 	{ MODKEY,                       XK_equal,  incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_minus,  incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_Left,   setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_Right,  setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ControlMask,           XK_c,      killclient,     {0} },
-	{ MODKEY|ShiftMask,             XK_b,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_t,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[2]} },
-	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[3]} },
-	{ MODKEY|ShiftMask,             XK_h,      setlayout,      {.v = &layouts[4]} },
-	{ MODKEY|ShiftMask,             XK_o,      setlayout,      {.v = &layouts[5]} },
-	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
-	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	{ MODKEY|ShiftMask,             XK_r,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_q,      spawn,          {.v = killcmd } },
+
+// LAYOUTS
+	{ MODKEY|ShiftMask,             XK_c,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY|ShiftMask,             XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[2]} },
+//	{ MODKEY|ShiftMask,             XK_m,      setlayout,      {.v = &layouts[3]} },
+//	{ MODKEY|ShiftMask,             XK_h,      setlayout,      {.v = &layouts[4]} },
+//	{ MODKEY|ShiftMask,             XK_o,      setlayout,      {.v = &layouts[5]} },
+
+	{ MODKEY,                       XK_space,  setlayout,      {0} },
+
+// TAGS / MONITORS
+	// { MODKEY,                       XK_0,      view,           {.ui = ~0 } },
+	// { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
+	// { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
+	// { MODKEY,                       XK_period, focusmon,       {.i = +1 } },
+	// { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
+	// { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	// TAGKEYS(                        XK_1,                      0)
+	// TAGKEYS(                        XK_2,                      1)
+	// TAGKEYS(                        XK_3,                      2)
+	// TAGKEYS(                        XK_4,                      3)
 };
 
 /* button definitions */
